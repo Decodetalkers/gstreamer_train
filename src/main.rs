71121fec150e11bb5f121a97419db1ff32a3f108
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-04-16 00:05:11 +0800

    chore: use add_many

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,31 @@ use ashpd::{
     WindowIdentifier,
 };
 //use gstreamer::{prelude::ObjectExt, traits::ElementExt, MessageType};
-use gstreamer::{traits::ElementExt, MessageType};
+use gstreamer::{prelude::{GstBinExtManual, ElementExtManual}, traits::ElementExt, MessageType};
 //use gstreamer::{traits::ElementExt, MessageType, prelude::GstBinExtManual};
 use std::os::unix::io::AsRawFd;
 
 fn screen_grstreamer<F: AsRawFd>(fd: F, node_id: Option<u32>) -> anyhow::Result<()> {
     gstreamer::init()?;
     let raw_fd = fd.as_raw_fd();
-    //let pipewire_element = gstreamer::ElementFactory::make("pipewiresrc").build()?;
-    //pipewire_element.set_property("fd", &raw_fd);
+    let element = gstreamer::Pipeline::new(None);
+    let videoconvert = gstreamer::ElementFactory::make("videoconvert").build()?;
+    let ximagesink = gstreamer::ElementFactory::make("ximagesink").build()?;
     if let Some(node) = node_id {
-        let pipewire_element = gstreamer::parse_launch(&format!(
-            "pipewiresrc fd={raw_fd} path={node} ! videoconvert ! ximagesink"
-        ))?;
-        //pipewire_element.set_property("path", &node.to_string());
-        pipewire_element.set_state(gstreamer::State::Playing)?;
-        let bus = pipewire_element.bus();
+        let pipewire_element = gstreamer::ElementFactory::make("pipewiresrc")
+            .property("fd", &raw_fd)
+            .property("path", &node.to_string())
+            .build()?;
+        element.add_many(&[&pipewire_element, &videoconvert, &ximagesink])?; 
+        pipewire_element.link(&videoconvert)?;
+        videoconvert.link(&ximagesink)?;
+        element.set_state(gstreamer::State::Playing)?;
+        let bus = element.bus();
         let message = bus
             .unwrap()
             .timed_pop_filtered(None, &[MessageType::Error, MessageType::Eos]);
         dbg!(message);
-        pipewire_element.set_state(gstreamer::State::Null)?;
+        element.set_state(gstreamer::State::Null)?;
     }
 
     Ok(())
