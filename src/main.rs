5c45adf45730fd2a56da22d99c86e2aa7c40b9a3
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-11-11 11:47:38 +0800

    chore: tidy up

--- src/main.rs
+++ src/main.rs
@@ -9,48 +9,43 @@ use gstreamer::{
     MessageType,
 };
 //use gstreamer::{traits::ElementExt, MessageType, prelude::GstBinExtManual};
-use std::os::unix::io::AsRawFd;
 
-fn screen_gstreamer<F: AsRawFd>(fd: F, node_id: Option<u32>) -> anyhow::Result<()> {
+fn screen_gstreamer(node: u32) -> anyhow::Result<()> {
     gstreamer::init()?;
-    let raw_fd = fd.as_raw_fd();
     let element = gstreamer::Pipeline::new();
     let videoconvert = gstreamer::ElementFactory::make("videoconvert").build()?;
     let ximagesink = gstreamer::ElementFactory::make("ximagesink").build()?;
-    if let Some(node) = node_id {
-        let pipewire_element = gstreamer::ElementFactory::make("pipewiresrc")
-            .property("fd", &raw_fd)
-            .property("path", &node.to_string())
-            .build()?;
-        element.add_many(&[&pipewire_element, &videoconvert, &ximagesink])?;
-        pipewire_element.link(&videoconvert)?;
-        videoconvert.link(&ximagesink)?;
-        element.set_state(gstreamer::State::Playing)?;
-        let bus = element.bus().unwrap();
-        loop {
-            let message = bus.timed_pop_filtered(
-                Some(gstreamer::ClockTime::from_useconds(1)),
-                &[MessageType::Error, MessageType::Eos],
-            );
-            if let Some(message) = message {
-                println!("Here is message");
-                match message.type_() {
-                    MessageType::Eos => {
-                        println!("End");
-                        break;
-                    }
-                    MessageType::Error => {
-                        println!("{:?}", message);
-                        println!("Error");
-                        break;
-                    }
-                    _ => continue,
+    let pipewire_element = gstreamer::ElementFactory::make("pipewiresrc")
+        .property("path", &node.to_string())
+        .build()?;
+    element.add_many([&pipewire_element, &videoconvert, &ximagesink])?;
+    pipewire_element.link(&videoconvert)?;
+    videoconvert.link(&ximagesink)?;
+    element.set_state(gstreamer::State::Playing)?;
+    let bus = element.bus().unwrap();
+    loop {
+        let message = bus.timed_pop_filtered(
+            Some(gstreamer::ClockTime::from_useconds(1)),
+            &[MessageType::Error, MessageType::Eos],
+        );
+        if let Some(message) = message {
+            println!("Here is message");
+            match message.type_() {
+                MessageType::Eos => {
+                    println!("End");
+                    break;
                 }
+                MessageType::Error => {
+                    println!("{:?}", message);
+                    println!("Error");
+                    break;
+                }
+                _ => continue,
             }
         }
-
-        element.set_state(gstreamer::State::Null)?;
     }
+
+    element.set_state(gstreamer::State::Null)?;
 
     Ok(())
 }
@@ -74,12 +69,11 @@ async fn main() -> anyhow::Result<()> {
         .start(&session, &WindowIdentifier::default())
         .await?
         .response()?;
-    let fd = proxy.open_pipe_wire_remote(&session).await?;
     response.streams().iter().for_each(|stream| {
         println!("node id: {}", stream.pipe_wire_node_id());
         println!("size: {:?}", stream.size());
         println!("position: {:?}", stream.position());
-        screen_gstreamer(fd, Some(stream.pipe_wire_node_id())).unwrap();
+        screen_gstreamer(stream.pipe_wire_node_id()).unwrap();
     });
     Ok(())
 }
